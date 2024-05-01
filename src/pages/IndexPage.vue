5880ccce41bc89c81a18688ea3e7ae21aeaacbc5
Gülzade Evni <glzd_75@hotmail.com>
2024-05-01 13:00:49 +0300

    add other files

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, watch } from 'vue';
+import { GoogleMap, Marker, InfoWindow } from 'vue3-google-map';
+
+const center = { lat: -25.363, lng: 131.044 };
+const infowindow = ref(true); // Will be open when mounted
+
+watch(infowindow, (v) => {
+  alert('infowindow has been ' + (v ? 'opened' : 'closed'));
+});
+</script>
+
+<template>
+<div>
+asdass
+
+  <GoogleMap
+    api-key="AIzaSyAj4VqWs_qq9pYVdl3kYCgy5bUIShMMskQ"
+    style="width: 100%; height: 500px"
+    :center="center"
+    :zoom="4"
+  >
+    <Marker :options="{ position: center }">
+      <InfoWindow v-model="infowindow">
+        <div id="content">This is the infowindow content</div>
+      </InfoWindow>
+    </Marker>
+  </GoogleMap>
+  </div>
+</template>
+
+
+//AIzaSyAj4VqWs_qq9pYVdl3kYCgy5bUIShMMskQ
